2c6af403b31dfb9899818d75f7881e71617202eb
Greg Provo <provo1@lsu.edu>
2021-11-30 14:24:43 -0600

    First Push

---
+++ Java/src/ComboLock.java
@@ -0,0 +1,21 @@
+public class ComboLock {
+    public static void main(String[] args) {
+        private int digit_1;
+        private int digit_2;
+        private int digit_3;
+        System.out.println("Test");
+    }
+
+    private static void setCombo(int digit_1 int digit_2 int digit_3) { }
+
+    private static void unlock(int digit_1 int digit_2 int digit_3) { }
+
+    private static void resetCombo(int digit_1 int digit_2 int digit_3) { }
+
+    private static void exit() {
+        System.out.println("Terminated");
+        System.exit(0);
+    }
+}
+
+
